127793b7f3b52fa3ce40d1b3274adde945b09417
osawa-koki <github@osawa.dev>
2023-02-26 13:41:29 +0900

    小さな修正。

--- src/HelloWorld/Function.cs
+++ src/HelloWorld/Function.cs
@@ -13,37 +13,37 @@ using Amazon.Lambda.APIGatewayEvents;
 namespace HelloWorld
 {
 
-    public class Function
+  public class Function
+  {
+
+    private static readonly HttpClient client = new HttpClient();
+
+    private static async Task<string> GetCallingIP()
+    {
+      client.DefaultRequestHeaders.Accept.Clear();
+      client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
+
+      var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
+
+      return msg.Replace("\n","");
+    }
+
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
     {
 
-        private static readonly HttpClient client = new HttpClient();
+      var location = await GetCallingIP();
+      var body = new Dictionary<string, string>
+      {
+        { "message", "hello world" },
+        { "location", location }
+      };
 
-        private static async Task<string> GetCallingIP()
-        {
-            client.DefaultRequestHeaders.Accept.Clear();
-            client.DefaultRequestHeaders.Add("User-Agent", "AWS Lambda .Net Client");
-
-            var msg = await client.GetStringAsync("http://checkip.amazonaws.com/").ConfigureAwait(continueOnCapturedContext:false);
-
-            return msg.Replace("\n","");
-        }
-
-        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
-        {
-
-            var location = await GetCallingIP();
-            var body = new Dictionary<string, string>
-            {
-                { "message", "hello world" },
-                { "location", location }
-            };
-
-            return new APIGatewayProxyResponse
-            {
-                Body = JsonSerializer.Serialize(body),
-                StatusCode = 200,
-                Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
-            };
-        }
+      return new APIGatewayProxyResponse
+      {
+        Body = JsonSerializer.Serialize(body),
+        StatusCode = 200,
+        Headers = new Dictionary<string, string> { { "Content-Type", "application/json" } }
+      };
     }
+  }
 }
